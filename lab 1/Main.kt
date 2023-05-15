326de66c8fd896f61a4b943595ac5118fb48b320
dev-jlpt18 <jlpt.1910@gmail.com>
2023-05-15 19:46:30 -0400

    Comentarios y mejora de la estructura de cada funcion

--- lab 1/Main.kt
+++ lab 1/Main.kt
@@ -1,8 +1,13 @@
+/*  Función que verifica la eficiencia de los algoritmos 
+    InsertionSort, BubbleSort, ShellSort y SelectionSort
+*/
 fun main(args: Array<String>) {
+    // Verificar si la entrada de datos esta completa
     if(args.size != 6){
 		println("Error, falta un argumento")
 		return
 	}
+    // Asignación de datos a las variables
     var t: Int = 0
     var s: String = ""
     var n: Int = 0
@@ -13,47 +18,60 @@ fun main(args: Array<String>) {
             "-s" -> s = args[i+1]
         }
     }
-    var secuencia: Array<Int> = generador(s, n)
-    println("Tenemos que: ${t}, ${s} y ${n}")
-    // println("La secuencia es: " + secuencia.contentToString()) 
+    // Verificar si los datos (intentos y cantidad de elementos) son validos
+    if (t < 1) {
+        println("Error, número de intentos no válido")
+        return
+    }
+    if (n < 1) {
+        println("Error, tamaño de la secuencia no válido")
+        return
+    }
+    // Generación de la secuencia y verificación de existencia de la clase de secuencia
+    var secuencia: Array<Int> = generadorDeSecuecias(s, n)
     if(secuencia[0] == -1){
 		println("Error, la clase de secuencia indicada no existe")
 		return
 	}
-
+    println("Tenemos que se realizarán ${t} intentos, la secuencia es de clase ${s} y contiene ${n} elementos")
+    // Chequeo de la eficiencia de cada algoritmo Sort
     for (i in 1 until 5) {
         var check = eficiencia(secuencia, t, i)
         if (check == -1) {
             return
-        } else if(check == -2) {
-            println("Error, número de intentos no válido")
         }
     }
 }
 
-fun generador(id: String, tamaño: Int): Array<Int> {
+// Función que genera la secuencia dependiendo de la clase indicada 
+fun generadorDeSecuecias(id: String, tamaño: Int): Array<Int> {
     var secuencia = Array<Int>(tamaño,{-1})
     if (id == "random") {
+        // Genera una secuencia de N elementos, de manera aleatoria
         for (i in 0 until tamaño) {
             secuencia[i] = (0..tamaño).random()
         }
         return secuencia
     } else if (id == "sorted") {
+        // Genera una secuencia de N elementos, con la forma [1,2,...N]
         for (i in 0 until tamaño) {
             secuencia[i] = i+1
         }
         return secuencia
     } else if (id == "inv") {
+        // Genera una secuencia de N elementos, con la forma [N...,2,1]
         for (i in 0 until tamaño) {
             secuencia[i] = tamaño-i
         }
         return secuencia
     } else if (id == "zu") {
+        // Genera una secuencia de N elementos, de manera aleatoria usando solo 1 y 0
         for (i in 0 until tamaño) {
             secuencia[i] = (0..1).random()
         }
         return secuencia
     } else if (id == "media") {
+        // Genera una secuencia de N elementos, con la forma [1,2,...[N/2],[N/2]...,2,1]
         for (i in 0 until tamaño) {
             if (i < tamaño/2) {
                 secuencia[i] = i+1 
@@ -63,10 +81,12 @@ fun generador(id: String, tamaño: Int): Array<Int> {
         }
         return secuencia
     } else {
+        // Genera una secuencia de N elementos, con solo (-1) como elementos
         return secuencia
     }
 }
 
+// Función que verifica si la secuencia esta ordenado de manera ascendente
 fun estaEnOrdenAscendente(A: Array<Int>): Boolean {
     for (i in 0 until (A.size-1)) {
         if (A[i] > A[i+1]) {
@@ -76,16 +96,18 @@ fun estaEnOrdenAscendente(A: Array<Int>): Boolean {
     return true
 }
 
+// Funcion que obtiene el promedio de una secuencia de números
 fun obtenerPromedioSecuencia(A: Array<Double>): Double {
 	var suma: Double = 0.0
 	for (i in 0 until A.size){
 		suma = suma + A[i]
-	}
+	} 
 	var n: Int = A.size
 	var promedio: Double = suma/n
 	return promedio 
 }
 
+// Función que obtiene la Derivación Estandar de una secuencia
 fun obtenerDesviacionEstandarSecuencia(A: Array<Double>): Double {
 	var promedio: Double = obtenerPromedioSecuencia(A)
 	var suma: Double = 0.0
@@ -99,7 +121,8 @@ fun obtenerDesviacionEstandarSecuencia(A: Array<Double>): Double {
 
 }
 
-fun sort(tipo: Int): String {
+// Función que determina cual algoritmo Sort se usara
+fun obtenerAlgoritmoSort(tipo: Int): String {
     when (tipo){
         1 -> return "InsertionSort"
         2 -> return "BubbleSort"
@@ -109,7 +132,7 @@ fun sort(tipo: Int): String {
     return "null"
 }
 
-
+// Función que mide el tiempo del algoritmo en segundos
 fun medirTiempo(A: Array<Int>, x: Int): Double {
 	var tInicio: Long = System.currentTimeMillis()
 	when (x){
@@ -123,13 +146,13 @@ fun medirTiempo(A: Array<Int>, x: Int): Double {
     }
 	var tFinal: Long = System.currentTimeMillis()
 	var tiempo: Double = ((tFinal- tInicio)/1000.0).toDouble()
-    // println("Insertion Sort: " + A.contentToString())
 	return tiempo
 }
 
+// Función que determina la eficiencia(tiempo e intentos) del algoritmo Sort seleccionado
 fun eficiencia(A: Array<Int>, intentos: Int, algoritmo: Int): Int {
-	var id: String = sort(algoritmo)
-    if(intentos > 1){
+	var id: String = obtenerAlgoritmoSort(algoritmo)
+    if(intentos > 1) {
 		var tiempos: Array<Double> = Array(intentos, {0.0})
 		for (i in 0 until intentos) {
 			tiempos[i] = medirTiempo(A, algoritmo)
@@ -141,21 +164,20 @@ fun eficiencia(A: Array<Int>, intentos: Int, algoritmo: Int): Int {
 		val tiempoPromedio: Double = obtenerPromedioSecuencia(tiempos)
 		val desviacionEstandar: Double = obtenerDesviacionEstandarSecuencia(tiempos)
 		println("Algoritmo: ${id}.")
-        println("Tiempo promedio: ${tiempoPromedio}")
-        println("Desviación estándar: ${desviacionEstandar}")
+        println("Tiempo promedio: ${tiempoPromedio} segundos")
+        println("Desviación estándar: ${desviacionEstandar} segundos")
     	println("¡Todos los ordenamientos fueron exitosos!")
+        println("")
     	return 1
-    } else if (intentos == 1){
+    } else {
     	var tiempo: Double = medirTiempo(A, algoritmo)
         if (tiempo == -1.0) {
             println("La secuencia no esta ordenada")
             return -1
         } else {
-            println("Algoritmo: ${id}. Tiempo: ${tiempo}")
+            println("Algoritmo: ${id}. Tiempo: ${tiempo} segundos")
     	    println("¡Ordenamiento exitoso!")
             return 1  
         }
-    } else {
-    	return -2
-    }
+    } 
 }
