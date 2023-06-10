61c66e1cc9c34a2503172b09a64d7560d8ceb8c9
dev-jlpt18 <jlpt.1910@gmail.com>
2023-06-09 21:16:04 -0400

    agregado a la libreria sort

--- lab 5/Sortlib.kt
+++ lab 5/Sortlib.kt
@@ -485,3 +485,84 @@ fun quicksortDualPivotOriginal(A: Array<Int>, left: Int, right: Int) {
 		quicksortDualPivotOriginal(A,g+1,right)
 	}
 }
+
+fun countingSort(A: Array<Int>) {
+	var n = A.size
+	var k = obtenerMaximo(A)
+	var B = Array(n, {0})
+	countingSortOriginal(A,B,k)
+	for (i in 0 until n) {
+		A[i] = B[i]
+	}
+}
+
+fun obtenerMaximo(A: Array<Int>): Int {
+	var n = A.size
+	var maximo = A[0]
+	for (i in 1 until n) {
+		if (A[i] > maximo) {
+			maximo = A[i]
+		}
+	}
+	return maximo
+}
+
+fun countingSortOriginal(A: Array<Int>, B: Array<Int>, k: Int) {
+	var n = A.size
+	var C = Array(k+1,{0})
+	for (j in 0 until n) {
+		C[A[j]] = C[A[j]] + 1
+	}
+	for (i in 1 until k+1) {
+		C[i] = C[i] + C[i-1]
+	}
+	for (j in n-1 downTo 0) {
+		B[C[A[j]]-1] = A[j]
+		C[A[j]] = C[A[j]] - 1
+	}
+}
+
+fun radixSort(A: Array<Int>) {
+	var n = A.size
+	var k = obtenerMaximo(A)
+	var d = obtenerNumeroDigitos(k)
+	var potenciaDe10 = 1
+	for (i in 0 until d+1) {
+		var B = Array(n, {0})
+		countingSortDigitos(A,B,9,potenciaDe10)
+		potenciaDe10 = 10*potenciaDe10
+		for (j in 0 until n) {
+			A[j] = B[j]
+		}
+	}
+}
+
+fun obtenerNumeroDigitos(a: Int): Int {
+	if (a == 0) {
+		return 1
+	}
+	var potenciaDe10 = 10
+	while(a/potenciaDe10 > 0) {
+		potenciaDe10 = 10*potenciaDe10
+	}
+	var exponentePotenciaDe10 = Math.log(potenciaDe10.toDouble())/Math.log(10.0)
+	var numeroDeDigitos = exponentePotenciaDe10.toInt()
+	return numeroDeDigitos
+}
+
+fun countingSortDigitos(A: Array<Int>, B: Array<Int>, k: Int, potenciaDe10: Int) {
+	var n = A.size
+	var C = Array(k+1,{0})
+	for (j in 0 until n) {
+		var digito = (A[j]/potenciaDe10)%10
+		C[digito] = C[digito] + 1
+	}
+	for (i in 1 until k+1) {
+		C[i] = C[i] + C[i-1]
+	}
+	for (j in n-1 downTo 0) {
+		var digito = (A[j]/potenciaDe10)%10
+		B[C[digito]-1] = A[j]
+		C[digito] = C[digito] - 1
+	}
+}
