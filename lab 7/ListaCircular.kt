8f27663cf2866fa4e499919347f45dde9d550baf
Haydee <haydeeccb@gmail.com>
2023-06-24 20:30:10 -0400

    Código Lista Circular. Falta arreglar método eliminar

---
+++ lab 7/ListaCircular.kt
@@ -0,0 +1,67 @@
+class ListaCircular {
+	
+	var Centinela: Nodo = Nodo(null)
+	var numeroNodos = 0
+	
+	init {
+		Centinela.prev = Centinela
+		Centinela.next = Centinela
+	}
+
+	fun agregarAlFrente(x: Nodo) {
+		x.next = Centinela.next
+		x.prev = Centinela
+		Centinela.next.prev = x
+		Centinela.next = x
+		numeroNodos = numeroNodos + 1 
+	}
+
+	fun agregarAlFinal(x: Nodo) {
+		x.next = Centinela
+		x.prev = Centinela.prev
+		Centinela.prev.next = x
+		Centinela.prev = x
+		numeroNodos = numeroNodos + 1 
+	}
+
+	fun buscar(k: Int): Nodo? {
+		Centinela.key = k
+		var x = Centinela.next
+		while(x.key != k) {
+			x = x.next 
+		}
+		Centinela.key = null
+		if (x == Centinela) {
+			return null
+		}
+		return x
+	}
+
+	fun eliminar(x: Nodo) {
+		if (x.estaInicializado() == true && x.key != null && x.prev != x && x.next != x) {
+			x.prev.next = x.next
+			x.next.prev = x.prev
+			numeroNodos = numeroNodos - 1
+			// El nodo eliminado se convierte en un "nodo solitario" que se llama a sí mismo
+			x.prev = x
+			x.next = x
+		}
+	}
+
+	override fun toString(): String {
+		var lista = ""
+		var x = Centinela.next
+		while(x.key != null) {
+			lista = lista+" ${x.key} "
+			x = x.next 
+		}
+		return lista 
+	}
+
+	fun buscarYEliminar(k: Int) {
+		var y = buscar(k)
+		if(y != null) {
+			eliminar(y)
+		} 
+	}
+}
