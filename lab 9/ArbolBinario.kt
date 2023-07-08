2775164cdd0c05a1f07eacc81814366afa5ea632
dev-jlpt18 <jlpt.1910@gmail.com>
2023-07-08 12:37:13 -0400

    version 2.0

--- lab 9/ArbolBinario.kt
+++ lab 9/ArbolBinario.kt
@@ -1,18 +1,3 @@
-/*fun main() {
-    val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
-    var x = ArbolBinario()
-    var y = x.Nodos
-    for (i in 0 until y.size) {
-    var s = y[i]
-    var d = s.prev?.letra
-    var e = s.next?.letra
-      println(s?.letra)
-      println(d?.letra)
-      println(e?.letra)
-      println("---------")
-    }
-  }
-*/
 class ArbolBinario {
     val letters:Array<Char?> = arrayOf(null,'e','t','i','a','n','m','s','u','r','w','d','k','g','o','h','v','f','l','p','j','b','x','c','y','z','q')
     val Nodos:Array<Nodo?> = Array(letters.size){Nodo()}
@@ -44,8 +29,10 @@ class ArbolBinario {
                 } else if (i == 10 || i == 11 || i == 12 || i == 13) {
                     hijoD = 2*i
                     hijoIz = (2*i)-1
+
                     Nodos[hijoIz] = Nodo(letters[hijoIz])
                     Nodos[hijoD] = Nodo(letters[hijoD])
+
                     var padre = Nodos[i]
                     padre?.letra = letters[i]
                     padre?.next = Nodos[hijoD]
@@ -58,7 +45,9 @@ class ArbolBinario {
                 } else {
                     Nodos[hijoIz] = Nodo(letters[hijoIz])
                     Nodos[hijoD] = Nodo(letters[hijoD])
+
                     var padre = Nodos[i]
+                    
                     padre?.letra = letters[i]
                     padre?.next = Nodos[hijoD]
                     padre?.prev = Nodos[hijoIz]
